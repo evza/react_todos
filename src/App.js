e429afea110e257d96cf5b48be3c88d982211c7c
Your Name <you@example.com>
2021-08-16 22:37:04 +0300

    first home work

--- src/App.js
+++ src/App.js
@@ -1,40 +1,29 @@
 import './App.css';
-import { useState } from 'react'
+import { Todo } from './Todo'
+import { useState} from 'react'
  
 function App() {
- const [todo, setTodo] = useState({})
- const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState({})
+  const [todos, setTodos] = useState([]);
  
- function handleClick(event) {
- event.preventDefault()
- setTodos([...todos, todo])
- setTodo({text: ''})
- }
+  function handleClick(event) {
+    event.preventDefault()
+    setTodos([...todos, todo])
+    setTodo({text: ''})
+  }
  
- function handleChangeTodo(event) {
- setTodo({id: todos.length + 1, text: event.target.value})
- }
+  function handleChangeTodo(event) {
+    setTodo({id: todos.length + 1, text: event.target.value})
+  } 
  
- function handleDeleteTodo(todoToDelete) {
- 
- const filteredTodos = todos.filter(todo => todo.id !== todoToDelete.id )
- setTodos(filteredTodos)
- }
- 
- return (
- <>
- <form >
- <input onChange={handleChangeTodo} value={todo.text}></input>
- <button type="submit" onClick={handleClick}>Save todo</button>
- </form>
- 
- <ul>
- {todos.map((todo) => <>
- <li key={todo.id}>{todo.text}</li>
- <button onClick={() => handleDeleteTodo(todo)}>delete</button>
- </>)}
- </ul>
- </>
+  return (
+  <>
+    <form >
+    <input onChange={handleChangeTodo} value={todo.text}></input>
+    <button type="submit" onClick={handleClick}>Save todo</button>
+    </form>     
+    <Todo/>
+  </>
  );
 }
  
