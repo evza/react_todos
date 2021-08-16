3b72fc535134ba37e600463223f42b6973b144ab
Your Name <you@example.com>
2021-08-16 21:57:56 +0300

    todoToDelete

--- src/App.js
+++ src/App.js
@@ -2,27 +2,37 @@ import './App.css';
 import { useState } from 'react'
  
 function App() {
- const [todo, setTodo] = useState('')
+ const [todo, setTodo] = useState({})
  const [todos, setTodos] = useState([]);
  
- function handleSubmit(event) {
+ function handleClick(event) {
  event.preventDefault()
  setTodos([...todos, todo])
- setTodo('')
+ setTodo({text: ''})
  }
  
  function handleChangeTodo(event) {
- 
- setTodo(event.target.value)
+ setTodo({id: todos.length + 1, text: event.target.value})
  }
+ 
+ function handleDeleteTodo(todoToDelete) {
+ 
+ const filteredTodos = todos.filter(todo => todo.id !== todoToDelete.id )
+ setTodos(filteredTodos)
+ }
+ 
  return (
  <>
  <form >
- <input onChange={handleChangeTodo} value={todo}></input>
- <button onClick={handleSubmit}>Save todo</button>
+ <input onChange={handleChangeTodo} value={todo.text}></input>
+ <button type="submit" onClick={handleClick}>Save todo</button>
  </form>
+ 
  <ul>
- {todos.map((todo) => <li key={todo}>{todo}</li>)}
+ {todos.map((todo) => <>
+ <li key={todo.id}>{todo.text}</li>
+ <button onClick={() => handleDeleteTodo(todo)}>delete</button>
+ </>)}
  </ul>
  </>
  );
